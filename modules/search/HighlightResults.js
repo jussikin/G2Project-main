b2d19384976fbf6b5bbba822388c2da7335d978b
dakanji <dakanji@user.noreply.github.com>
2017-05-26 14:31:06 +0300

    Fix search

--- modules/search/HighlightResults.js
+++ modules/search/HighlightResults.js
@@ -11,13 +11,14 @@ function search_HighlightResults(criteria) {
 			if (spans[i].childNodes[j].nodeName == "#text") {
 				node = spans[i].childNodes[j];
 				result = node.nodeValue;
-			/* Some browsers (Firefox) unescape the node value, so re-escape as necessary */
+				
+				/* Some browsers (Firefox) unescape the node value, so re-escape as necessary */
 				result = result.replace(/</g, "&lt;");
 				result = result.replace(/>/g, "&gt;");
 				result = result.replace(/"/g, "&quot;");
+				
 				var spanEl = document.createElement("span");
-				spanEl.innerHTML =
-				result.replace(regex, "<span class=\"giSearchHighlight\">$1</span>");
+				spanEl.innerHTML = result.replace(regex, "<span class=\"giSearchHighlight\">$1</span>");
 				node.parentNode.insertBefore(spanEl, node);
 				node.parentNode.removeChild(node);
 			}
